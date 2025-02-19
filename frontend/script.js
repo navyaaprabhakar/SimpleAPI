2d52081a78f2a964c4aa5a6eba1673c17f354620
Navyaa Prabhakar <119664283+navyaaprabhakar@users.noreply.github.com>
2025-02-19 20:26:19 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000"; // Change after backend deployment
+const API_BASE_URL = "https://greeting-backend.vercel.app/"; // Change after backend deployment
 
 async function getGreeting() {
   const name = document.getElementById("nameInput").value.trim();
